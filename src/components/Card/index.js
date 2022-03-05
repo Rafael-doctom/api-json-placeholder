e2f6d2e00587e06ce179d0c11eda8768726e3c31
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-03-05 14:11:10 -0300

    feat: mediaQueries added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,29 +4,29 @@ import * as S from './style';
 
 export default function Body() {
 
-    const [dados, setDados] = useState()
+    const [dados, setDados] = useState();
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
             .then(response => response.json())
             .then(json => setDados(json))
-    }, [])
+    }, []);
 
     return (
         <>
-            {dados &&
-                dados.map(item => (
-                    <>
-                        <S.Container>
-                            <S.Card>
-                                <S.ID>{item.id}</S.ID>
-                                <S.Title>{item.title}</S.Title>
-                                <S.Paragraph>{item.body}</S.Paragraph>
-                            </S.Card>
-                        </S.Container>
-                    </>
-                ))
-            }
+                {dados &&
+                    dados.map(item => (
+                        <>
+                            <S.Container>
+                                <S.Card>
+                                    <S.ID>{item.id}</S.ID>
+                                    <S.Title>{item.title}</S.Title>
+                                    <S.Paragraph>{item.body}</S.Paragraph>
+                                </S.Card>
+                            </S.Container>
+                        </>
+                    ))
+                }
         </>
-    )
-}
+    );
+};
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,25 +1,64 @@
 import styled from 'styled-components';
+
 export const Container = styled.div`
-    display:flex;
+    margin:2rem auto;
 `;
+
 export const Card = styled.div`
-    width:20vw;
+    width:80vw;
     height:20vh;
     text-align:center;
     padding:1rem;
-    padding-top:5rem;
-    padding-bottom:5rem;
+    padding-top:3rem;
+    padding-bottom:4rem;
     background:tomato;
-    margin:1rem;
+    margin:1rem auto;
     color:white;
     border-radius:10px;
+
+    @media(max-width:768px){
+        width:80vw;
+        height:20vh;
+        padding-top:1rem;
+        padding-bottom:3rem;
+    }
 `;
+
 export const ID = styled.span`
 
 `;
+
 export const Title = styled.h3`
-
+    @media(max-width:375px){
+        font-size:1rem;
+    }
 `;
-export const Paragraph = styled.p`
 
+export const Paragraph = styled.p`
+    width:30vw;
+    margin:1rem auto;
+    text-align:center;
+
+    @media(max-width:768px){
+        width:50vw;
+        height:20vh;
+    }
+
+    @media(max-width:425px){
+        width:80vw;
+        height:20vh;
+        font-size:0.80rem;
+
+    }
+
+    @media(max-width:375px){
+        font-size:0.80rem;
+        width:70vw;
+        height:20vh;
+    }
+    @media(max-width:320px){
+        font-size:0.70rem;
+        width:70vw;
+        height:20vh;
+    }
 `;
